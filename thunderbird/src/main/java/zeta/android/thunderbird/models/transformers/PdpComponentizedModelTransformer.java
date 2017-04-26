6e49b4f9b3d30c479d7bd75e609b4a32bbd4dfb4
Manjunath Chandrashekar <manjunath.c23@gmail.com>
2017-04-26 22:21:17 +0530

    Transformer

--- thunderbird/src/main/java/zeta/android/thunderbird/models/transformers/PdpComponentizedModelTransformer.java
+++ thunderbird/src/main/java/zeta/android/thunderbird/models/transformers/PdpComponentizedModelTransformer.java
@@ -1,10 +1,12 @@
 package zeta.android.thunderbird.models.transformers;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import zeta.android.thunderbird.api.apify.pdpv3.componentization.PdpComponentizationCardsResponse;
+import zeta.android.thunderbird.api.apify.pdpv3.componentization.PdpComponentizationComponentsResponse;
 import zeta.android.thunderbird.api.apify.pdpv3.componentization.PdpComponentizationInfoResponse;
 import zeta.android.thunderbird.api.apify.pdpv3.componentization.PdpComponentizationResponse;
 import zeta.android.thunderbird.models.common.ITransformer;
@@ -18,7 +20,9 @@ import zeta.android.thunderbird.models.products.pdpv3.cards.PdpV3RelatedCard;
 import zeta.android.thunderbird.models.products.pdpv3.cards.PdpV3ServiceabilityCard;
 import zeta.android.thunderbird.models.products.pdpv3.cards.PdpV3SocialCard;
 import zeta.android.thunderbird.models.products.pdpv3.common.PdpV3CardType;
+import zeta.android.thunderbird.models.products.pdpv3.common.PdpV3ComponentType;
 import zeta.android.thunderbird.models.utils.PdpV3CardTypeUtil;
+import zeta.android.thunderbird.models.utils.PdpV3ProductComponentTypeUtil;
 
 import static zeta.android.thunderbird.models.products.pdpv3.common.PdpV3CardType.BRAND;
 import static zeta.android.thunderbird.models.products.pdpv3.common.PdpV3CardType.MORE_INFO_CARD;
@@ -42,18 +46,23 @@ public class PdpComponentizedModelTransformer implements ITransformer<PdpCompone
 
         PdpV3BrandCard brandCard = null;
         PdpV3SocialCard socialCard = null;
-        PdpV3ProductCard productCard = null;
         PdpV3RelatedCard relatedCard = null;
+        PdpV3ProductCard productCard = null;
         PdpV3MoreInfoCard moreInfoCard = null;
         PdpV3ServiceabilityCard serviceabilityCard = null;
+        @PdpV3CardType
+        LinkedHashMap<String, Integer> cardPositionIndex = new LinkedHashMap<>(0);
 
         //region card
         if (cardsList != null) {
             final int size = cardsList.size();
-            for (int i = 0; i < size; i++) {
-                final PdpComponentizationCardsResponse cardsResponse = cardsList.get(i);
+            for (int indexPos = 0; indexPos < size; indexPos++) {
+                final PdpComponentizationCardsResponse cardsResponse = cardsList.get(indexPos);
+
                 @PdpV3CardType
                 final String cardType = PdpV3CardTypeUtil.from(cardsResponse.type);
+                //Maintain the index of the card
+                cardPositionIndex.put(cardType, indexPos);
 
                 switch (cardType) {
                     case PRODUCT:
@@ -80,7 +89,6 @@ public class PdpComponentizedModelTransformer implements ITransformer<PdpCompone
             }
         }
         //endregion
-
         return PdpComponentizationModel.create()
                 .setProductId(productId)
                 .setProductTitle(title)
@@ -89,12 +97,30 @@ public class PdpComponentizedModelTransformer implements ITransformer<PdpCompone
                 .setProductCard(productCard)
                 .setRelatedCard(relatedCard)
                 .setMoreInfoCard(moreInfoCard)
+                .setCardPositionsIndex(cardPositionIndex)
                 .setServiceabilityCard(serviceabilityCard)
                 .build();
     }
 
     private PdpV3ProductCard transformProductCard(PdpComponentizationCardsResponse productCard) {
+        @PdpV3ComponentType
+        LinkedHashMap<String, Integer> componentIndex = new LinkedHashMap<>(0);
+        List<PdpComponentizationComponentsResponse> componentsList = productCard.componentsList;
+        if (componentsList != null) {
+            final int size = componentsList.size();
+            for (int indexPos = 0; indexPos < size; indexPos++) {
+                PdpComponentizationComponentsResponse componentsResponse = componentsList.get(indexPos);
+
+                //Maintain the index of the component
+                @PdpV3ComponentType
+                String componentType = PdpV3ProductComponentTypeUtil.from(componentsResponse.type);
+                componentIndex.put(componentType, indexPos);
+            }
+        }
+
         return PdpV3ProductCard.create()
+                .setPdpV3CardType(PdpV3CardType.PRODUCT)
+                .setComponentPositionsIndex(componentIndex)
                 .build();
     }
 
---
+++ thunderbird/src/main/java/zeta/android/thunderbird/models/utils/PdpV3ProductComponentTypeUtil.java
@@ -0,0 +1,51 @@
+package zeta.android.thunderbird.models.utils;
+
+import android.support.annotation.Nullable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import zeta.android.thunderbird.models.products.pdpv3.common.PdpV3ComponentType;
+import zeta.android.utils.lang.StringUtils;
+
+/**
+ * Product component utility
+ * In principle this util method should and must server only `product's` component type only
+ * rest everything will be "unknown"
+ */
+@ParametersAreNonnullByDefault
+public class PdpV3ProductComponentTypeUtil {
+
+    private static final String PRODUCT_COMPONENT_IMAGE = "PRODUCT_COMPONENT_IMAGE";
+    private static final String PRODUCT_COMPONENT_INFO = "INFO";
+    private static final String PRODUCT_COMPONENT_VAT_INFO = "PRODUCT_COMPONENT_VAT_INFO";
+    private static final String PRODUCT_COMPONENT_BEST_PRICE = "PRODUCT_COMPONENT_BEST_PRICE";
+    private static final String PRODUCT_COMPONENT_SIZE_SELECTOR = "PRODUCT_COMPONENT_SIZE_SELECTOR";
+    private static final String PRODUCT_COMPONENT_PRIMARY_OFFER = "PRODUCT_COMPONENT_PRIMARY_OFFER";
+    private static final String PRODUCT_COMPONENT_ADD_TO_CART_BUTTON = "PRODUCT_COMPONENT_ADD_TO_CART_BUTTON";
+
+    @PdpV3ComponentType
+    public static String from(@Nullable String identifierType) {
+        if (StringUtils.isNullOrEmpty(identifierType)) {
+            return PdpV3ComponentType.UNKNOWN;
+        }
+
+        switch (identifierType.toLowerCase()) {
+            case PRODUCT_COMPONENT_IMAGE:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_IMAGE;
+            case PRODUCT_COMPONENT_INFO:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_INFO;
+            case PRODUCT_COMPONENT_VAT_INFO:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_VAT_INFO;
+            case PRODUCT_COMPONENT_BEST_PRICE:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_BEST_PRICE;
+            case PRODUCT_COMPONENT_SIZE_SELECTOR:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_SIZE_SELECTOR;
+            case PRODUCT_COMPONENT_PRIMARY_OFFER:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_PRIMARY_OFFER;
+            case PRODUCT_COMPONENT_ADD_TO_CART_BUTTON:
+                return PdpV3ComponentType.PRODUCT_COMPONENT_ADD_TO_CART_BUTTON;
+            default:
+                return PdpV3ComponentType.UNKNOWN;
+        }
+    }
+}
