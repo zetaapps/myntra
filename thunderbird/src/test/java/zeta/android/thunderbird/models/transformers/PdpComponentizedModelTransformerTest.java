92181781a14adaf8b14c2b007f856a7446c7d21f
Manjunath Chandrashekar <manjunath.c23@gmail.com>
2017-05-03 08:01:20 +0530

    Reslove conflicts

--- thunderbird/src/test/java/zeta/android/thunderbird/models/transformers/PdpComponentizedModelTransformerTest.java
+++ thunderbird/src/test/java/zeta/android/thunderbird/models/transformers/PdpComponentizedModelTransformerTest.java
@@ -1,16 +1,11 @@
 package zeta.android.thunderbird.models.transformers;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Testing template
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import zeta.android.thunderbird.api.apify.pdpv3.componentization.PdpComponentizationResponse;
-<<<<<<< HEAD
 import zeta.android.thunderbird.models.common.GenderType;
 import zeta.android.thunderbird.models.products.common.ProductBrand;
 import zeta.android.thunderbird.models.products.pdpv3.PdpComponentizationModel;
@@ -46,38 +41,4 @@ public class PdpComponentizedModelTransformerTest {
                 PdpComponentizationResponse.class);
         return mPdpModelTransformer.transform(response);
     }
-
-=======
-public class PdpComponentizedModelTransformerTest {
-
->>>>>>> Little clean up
-=======
-import zeta.android.thunderbird.models.products.pdpv3.PdpComponentizationModel;
-import zeta.android.thunderbird.utils.TestUtils;
-
-@ParametersAreNonnullByDefault
-public class PdpComponentizedModelTransformerTest {
-
-    private PdpComponentizedModelTransformer mPdpModelTransformer;
-
-    @Before
-    public void setUp() throws Exception {
-        mPdpModelTransformer = new PdpComponentizedModelTransformer();
-    }
-
-    @Test
-    public void testPdpResponse() {
-        PdpComponentizationModel pdpResponse = getPdpComponentizationModel();
-        assert pdpResponse != null;
-        //TODO :: Add more asserts here.
-        //TODO:: Test unit level here.
-    }
-
-    private PdpComponentizationModel getPdpComponentizationModel() {
-        PdpComponentizationResponse response = TestUtils.deserialize("pdp_v3_response_1675810_componentized_response.json",
-                PdpComponentizationResponse.class);
-        return mPdpModelTransformer.transform(response);
-    }
-
->>>>>>> Testing template
 }
